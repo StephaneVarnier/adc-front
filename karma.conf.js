6927615d88452c36d3ad179096b9be5f21cd0644
var.stephane <varnier.s@orange.fr>
2020-07-20 01:36:27 +0200

    from headless to no sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -25,17 +25,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeNoSandbox'],
     customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          // Without a remote debugging port, Google Chrome exits immediately.
-          '--remote-debugging-port=9222',
-        ],
-      }
+        ChromeNoSandbox: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
     },
     singleRun: true,
     restartOnFileChange: true
