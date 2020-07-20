03f09f66567cffc4ae33ab8ffa434a6695ae76fd
var.stephane <varnier.s@orange.fr>
2020-07-20 15:29:09 +0200

    karma

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+          base: 'Chrome',
+          flags: [
+              '--headless',
+              '--disable-gpu',
+              // Without a remote debugging port, Google Chrome exits immediately.
+              '--remote-debugging-port=9222'
+          ],
+          debug: true
+      }
+  },
     concurrency: Infinity,
     restartOnFileChange: true
   });
