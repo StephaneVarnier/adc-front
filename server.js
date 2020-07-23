a29a5cfc8c41e2630516f1bd6f4cf62411ca735b
var.stephane <varnier.s@orange.fr>
2020-07-23 16:54:00 +0200

    cors issue

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static('./dist/adc-front'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.get('/*', function(req, res) {
     res.sendFile('index.html', {root: 'dist/adc-front/'}
   );
